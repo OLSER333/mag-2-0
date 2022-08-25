9589ccf26a0d3e42124c31eab893cbd1c50cd3ea
olser <gritsienko.oleg@mail.ru>
2022-08-25 09:11:41 +0300

    звёзды, адаптив снова

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 import '../scss/style.scss';
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').slice(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
+
+    })
+  })
+}
 
 
 const data = {
